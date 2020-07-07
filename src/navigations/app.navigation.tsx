f083cd76a638e22e7e185ff0567785eb2944ef85
huseyinsaritas <32297624+hsynsrts@users.noreply.github.com>
2020-07-07 23:57:51 +0300

    Recommendation set navigation

--- src/navigations/app.navigation.tsx
+++ src/navigations/app.navigation.tsx
@@ -1,29 +1,21 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
 import Intro from "../scenes/intro.scene";
 import ChooseAgeRange from "../scenes/choose-age-range.scene";
 import ChooseFavorites from "../scenes/choose-favorites.scene";
+import Recommendation from "../scenes/recommendation.scene";
 
 export const AppNavigation = () => (
-
-    <NavigationContainer>
-
-        <Stack.Navigator headerMode="none" initialRouteName="Intro">
-            <Stack.Screen
-                name="Intro"
-                component={Intro} />
-            <Stack.Screen
-                name="ChooseAgeRange"
-                component={ChooseAgeRange} />
-            <Stack.Screen
-                name="ChooseFavorites"
-                component={ChooseFavorites} />
-        </Stack.Navigator>
-
-    </NavigationContainer>
-
-)
+  <NavigationContainer>
+    <Stack.Navigator headerMode="none" initialRouteName="Intro">
+      <Stack.Screen name="Intro" component={Intro} />
+      <Stack.Screen name="ChooseAgeRange" component={ChooseAgeRange} />
+      <Stack.Screen name="ChooseFavorites" component={ChooseFavorites} />
+      <Stack.Screen name="Recommendation" component={Recommendation} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
