f875efc0c5c4fefc9876dc02ffa18678afc4183d
huseyinsaritas <32297624+hsynsrts@users.noreply.github.com>
2020-07-08 22:54:42 +0300

    Button component renamed

---
+++ src/components/Button.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+
+import { COLORS, FONTS } from "../styles/styles";
+
+const Button = ({ onPress, title, width }: any) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={[styles.button, width]} activeOpacity={0.8}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: COLORS.red,
+    borderRadius: 6,
+    paddingHorizontal: 20,
+    paddingVertical: 8
+  },
+  text: {
+    fontSize: 13,
+    color: COLORS.white,
+    fontFamily: FONTS.bungee
+  }
+});
+
+export default Button;
