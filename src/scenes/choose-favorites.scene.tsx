fe1567a09d84855c2a7004842fe38b6a44ddb98b
huseyinsaritas <32297624+hsynsrts@users.noreply.github.com>
2020-07-09 22:47:18 +0300

    added SearchBar and text on the images

--- src/scenes/choose-favorites.scene.tsx
+++ src/scenes/choose-favorites.scene.tsx
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity, FlatList, Image } from "react-native";
-
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, ImageBackground } from "react-native";
 import { COLORS, FONTS, LAYOUT } from "../styles/styles";
 import Button from "../components/Button";
+import SearchBar from "../components/SearchBar";
 import FILMS from "../data/films100.json";
 
 const ChooseFavorites = ({ navigation }: any) => {
   const [selectedFilms, setSelectedFilms] = useState([] as string[]);
+  const [search, setSearch] = useState<string>("");
 
   const FilmItem = ({ film }: any) => {
     const selected = selectedFilms.includes(film.id);
 
     return (
       <TouchableOpacity style={[styles.film, selected ? styles.filmSelected : {}]} onPress={() => selectFilm(film.id)} activeOpacity={0.8}>
-        {selected && <Image style={styles.filmSeletedIcon} source={require("../assets/selected.png")} />}
-        <Image style={styles.filmPoster} source={{ uri: film.image }} />
+        {selected && <ImageBackground style={styles.filmSeletedIcon} source={require("../assets/selected.png")} />}
+        <ImageBackground style={styles.filmPoster} source={{ uri: film.image }}>
+          <Text style={styles.filmTitle}>{film.title}</Text>
+        </ImageBackground>
       </TouchableOpacity>
     );
   };
@@ -38,6 +41,7 @@ const ChooseFavorites = ({ navigation }: any) => {
     <View style={[LAYOUT, styles.layout]}>
       <Text style={styles.pageTitle}>Favori Seçin</Text>
       <Text style={styles.pageDesc}>Biraz yardımcı olmanız için en az {selectedFilms.length}/3 tane beğendiğiniz film veya diziyi işaretleyin.</Text>
+      <SearchBar value={search} onChangeText={input => setSearch(input)} placeholder="Ara..." />
       <FlatList data={FILMS} style={styles.films} numColumns={3} renderItem={({ item }) => <FilmItem film={item} />} keyExtractor={item => item.id} />
       {selectedFilms.length > 2 ? <Button title="TAVSİYE AL" onPress={onPress} /> : null}
     </View>
@@ -79,7 +83,19 @@ const styles = StyleSheet.create({
     height: 160,
     resizeMode: "cover",
     borderRadius: 5,
-    overflow: "hidden"
+    overflow: "hidden",
+    position: "relative"
+  },
+  filmTitle: {
+    fontWeight: "bold",
+    color: "white",
+    position: "absolute",
+    bottom: 0,
+    left: 2,
+    fontSize: 12,
+    textShadowColor: "rgba(0, 0, 0, 0.75)",
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10
   },
   filmSeletedIcon: {
     position: "absolute",
